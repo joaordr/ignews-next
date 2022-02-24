a3a3302868d5a653e2625414d2c55d683375cb23
João Rodrigo Prieto <jrp2033@gmail.com>
2022-02-23 23:56:42 -0300

    fix: nextAuth secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,7 +6,7 @@ import GithubProvider from "next-auth/providers/github"
 import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
-    secret: 'sNTDXDz24Q4pOhtA8ahG9m4pzx4UQiCgSsOf31kOW/g=',
+    secret: process.env.JWT_SECRET,
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
